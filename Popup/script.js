2bdfa8ce5681ebab3f29d231238a140b8d1297e7
Jannick Holm <jannick@wonderfestiwall.dk>
2019-02-14 11:44:42 +0100

    oprettet i repo

---
+++ Popup/script.js
@@ -0,0 +1,83 @@
+ document.addEventListener("DOMContentLoaded", start);
+
+ function start() {
+     let filter = "alle";
+     let sFilter = "";
+     let retter = [];
+
+
+     document.querySelectorAll(".filter").forEach(elm => {
+         elm.addEventListener("click", filtreing);
+     });
+
+
+     function filtreing() {
+         filter = this.getAttribute("data-ret");
+         document.querySelector("h1").textContent = this.textContent;
+         document.querySelectorAll(".filter").forEach(elm => {
+             elm.classList.remove("valgt");
+         });
+         this.classList.add("valgt");
+         console.log(filter);
+         visRetter();
+     }
+
+     function singleViewFilter() {
+         sFilter = this.getAttribute("data-retter");
+         console.log("Virk", this, sFilter);
+         location.href = "single.html?id=" + sFilter;
+     }
+
+
+
+     function fjernsingle() {
+         document.querySelector(".single").classList.add("hide");
+         document.querySelector(".overlay").classList.add("hide");
+     }
+
+
+     async function getJson() {
+         let jsonData = await fetch("https://mandalskeawebspace.dk/claude_php/clean_up_spreadsheet.php?id=1jxxxFoWBuMJ1qhQ9BQIAyKHmP38XtAF9_sQr0xo5JLo");
+         retter = await jsonData.json();
+         visRetter();
+     }
+
+
+
+
+
+     function visRetter() {
+         document.querySelector("#retter").innerHTML = "";
+         retter.forEach(retter => {
+             if (filter == "alle" || filter == retter.kategori) {
+                 document.querySelector("#retter").innerHTML +=
+
+                     `<div class="vis" data-retter="${retter.id}">
+
+<img src="imgs/large/${retter.billede}.jpg" alt="${retter.navn}">
+
+<div class="tekst2">
+ <h2>${retter.navn}</h2>
+<p>${retter.kort}</p>
+<p><b>Pris:</b> ${retter.pris},-</p>
+
+</div>  
+</div>`
+             }
+
+
+         });
+
+         document.querySelectorAll(".vis").forEach(elms => {
+             elms.addEventListener("click", singleViewFilter);
+         });
+
+
+
+     }
+
+     getJson();
+
+
+
+ }
